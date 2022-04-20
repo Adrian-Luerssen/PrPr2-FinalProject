689d49d9050d4cf41e7aa64c65a0f62c7b237fd4
Adrian-Luerssen <91116739+Adrian-Luerssen@users.noreply.github.com>
2022-04-20 22:57:11 +0200

    login and sign up movement completed and app icon created

--- Events App/app/src/main/java/com/example/events/LoginActivity.java
+++ Events App/app/src/main/java/com/example/events/LoginActivity.java
@@ -1,7 +1,14 @@
 package com.example.events;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,16 +17,34 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
+    public static final int SIGNED_UP = 1;
+
     private Button signIn;
+    private Button gotoSignUp;
     private EditText email;
     private EditText password;
     private Pattern emailPattern;
+
+    private final ActivityResultLauncher<Intent> activityLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            System.out.println("here");
+            Intent intent = result.getData();
+            if (result.getResultCode() == SIGNED_UP && intent != null) {
+
+                email.setText(intent.getStringExtra(getString(R.string.email)));
+
+            }
+        }
+    });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initFields();
-        loginButtonSetListener();
+        setButtonListener();
 
     }
 
@@ -30,14 +55,20 @@ public class LoginActivity extends AppCompatActivity {
         email = (EditText) findViewById(R.id.Login_email_input);
         password = (EditText) findViewById(R.id.Login_password_input);
         emailPattern = Pattern.compile("^(.+)@(.+)$");
+        gotoSignUp = (Button) findViewById(R.id.move_to_register_account);
     }
 
-    private void loginButtonSetListener() {
+    private void setButtonListener() {
         signIn.setOnClickListener(view -> {
             if (validEmail(email.getText().toString()) && password.getText().toString().length() != 0){
                 System.out.println("VALID");
             }
             //check if email and password are valid (API)
+        });
+
+        gotoSignUp.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SignUpActivity.class);
+            activityLauncher.launch(intent);
         });
     }
 
--- Events App/app/src/main/java/com/example/events/SignUpActivity.java
+++ Events App/app/src/main/java/com/example/events/SignUpActivity.java
@@ -2,13 +2,82 @@ package com.example.events;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    private Button signUp;
+    private Button gotoLogin;
+    private EditText email;
+    private EditText password;
+    private EditText phone;
+    private EditText firstName;
+    private EditText lastName;
+    private Pattern emailPattern;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        initFields();
+        setButtonListener();
+    }
+
+    private void setButtonListener() {
+        signUp.setOnClickListener(view -> {
+
+            if (validInputs()){
+                Intent intent = new Intent();
+                intent.putExtra(getString(R.string.email),email.getText().toString());
+                setResult(LoginActivity.SIGNED_UP,intent);
+                finish();
+            }
+
+        });
+
+        gotoLogin.setOnClickListener(view->{
+            setResult(LoginActivity.RESULT_CANCELED);
+            finish();
+        });
+    }
+
+    private boolean validInputs() {
+        if (validEmail(email.getText().toString()) && password.getText().toString().length() != 0){
+            System.out.println("email password fine");
+            if (firstName.getText().toString().length() != 0 && (firstName.getText().toString().split(" ")).length == 1 && lastName.getText().toString().length() != 0){
+                System.out.println("names fine");
+                if (validPhone(phone.getText().toString())){
+                    System.out.println("phone fine");
+                    return true;
+                }
+
+            }
+
+        }
+        return false;
+    }
+
+    private boolean validPhone(String phoneNumber) {
+        //CHECK PHONE NUMBER PLEASEE
+        return true;
+    }
+
+    private void initFields() {
+        signUp = (Button) findViewById(R.id.sign_up_button);
+        email = (EditText) findViewById(R.id.SignUp_email_input);
+        password = (EditText) findViewById(R.id.SignUp_password_input);
+        phone = (EditText) findViewById(R.id.SignUp_Phone_input);
+        firstName = (EditText) findViewById(R.id.SignUp_First_Name_input);
+        lastName = (EditText) findViewById(R.id.SignUp_Last_Name_input);
+        gotoLogin = (Button) findViewById(R.id.move_to_login);
+        emailPattern = Pattern.compile("^(.+)@(.+)$");
+    }
+
+    private boolean validEmail(String email){
+        return emailPattern.matcher(email).matches();
     }
 }
