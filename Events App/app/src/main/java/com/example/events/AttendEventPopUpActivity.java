8d2a3fa84f07d39629bdf93b9097324bbdceaa2e
nuriaensenat <nurialou.ensenat@students.salle.url.edu>
2022-04-24 12:45:38 +0200

    attending event pop up added

---
+++ Events App/app/src/main/java/com/example/events/AttendEventPopUpActivity.java
@@ -0,0 +1,14 @@
+package com.example.events;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AttendEventPopUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attend_event_pop_up);
+    }
+}
