3d720e1ecb224e251f7308ddc50e77ba566a69db
Adrian-Luerssen <91116739+Adrian-Luerssen@users.noreply.github.com>
2022-06-09 12:31:26 +0200

    starting on the chat

---
+++ Events App/app/src/main/java/com/example/events/Activities/OurMessagesActivity.java
@@ -0,0 +1,16 @@
+package com.example.events.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.events.R;
+
+public class OurMessagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_our_messages);
+    }
+}
