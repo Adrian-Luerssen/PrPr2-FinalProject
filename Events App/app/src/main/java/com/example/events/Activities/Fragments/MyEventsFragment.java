5e9851a4edbd68af63073fdaf689d0d31bcd6217
Ariadna González Navarro <ariadna.gonzalez@students.salle.url.edu>
2022-06-12 16:50:05 +0200

    star rate bar

--- Events App/app/src/main/java/com/example/events/Activities/Fragments/MyEventsFragment.java
+++ Events App/app/src/main/java/com/example/events/Activities/Fragments/MyEventsFragment.java
@@ -2,16 +2,19 @@ package com.example.events.Activities.Fragments;
 
 import androidx.fragment.app.Fragment;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.events.R;
 
 public class MyEventsFragment extends Fragment {
-
     private View view;
+    private TextView dropEvent;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,22 @@ public class MyEventsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.my_events_fragment, container, false);
+        initFields();
+        setButtonListener();
         return view;
+    }
+
+    private void initFields() {
+        dropEvent = (TextView) view.findViewById(R.id.dropEvent);
+    }
+
+    private void setButtonListener() {
+        dropEvent.setOnClickListener(view -> {
+            AlertDialog.Builder dialogBuilding = new AlertDialog.Builder(getContext());
+            final View attendPopUp = getLayoutInflater().inflate(R.layout.drop_event_pop_up, null);
+            dialogBuilding.setView(attendPopUp);
+            AlertDialog dialog = dialogBuilding.create();
+            dialogBuilding.show();
+        });
     }
 }
--- Events App/app/src/main/java/com/example/events/Activities/Fragments/RateEventFragment.java
+++ Events App/app/src/main/java/com/example/events/Activities/Fragments/RateEventFragment.java
@@ -2,16 +2,19 @@ package com.example.events.Activities.Fragments;
 
 import androidx.fragment.app.Fragment;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.events.R;
 
 public class RateEventFragment extends Fragment {
-
     private View view;
+    private Button rateEvent;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,22 @@ public class RateEventFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.rate_event_fragment, container, false);
+        initFields();
+        setButtonListener();
         return view;
+    }
+
+    private void initFields() {
+        rateEvent = (Button) view.findViewById(R.id.rate);
+    }
+
+    private void setButtonListener() {
+        rateEvent.setOnClickListener(view -> {
+            AlertDialog.Builder dialogBuilding = new AlertDialog.Builder(getContext());
+            final View attendPopUp = getLayoutInflater().inflate(R.layout.rating_bar_event, null);
+            dialogBuilding.setView(attendPopUp);
+            AlertDialog dialog = dialogBuilding.create();
+            dialogBuilding.show();
+        });
     }
 }
